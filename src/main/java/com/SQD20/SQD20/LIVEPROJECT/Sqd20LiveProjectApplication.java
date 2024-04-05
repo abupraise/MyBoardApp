41a92485b2f26e86fc6a8bbf99f70daf3b30913d
Deraclassic <chiderauzoigwe2@gmail.com>
2024-04-05 15:02:32 +0100

    innitial commit

---
+++ src/main/java/com/SQD20/SQD20/LIVEPROJECT/Sqd20LiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SQD20.SQD20.LIVEPROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sqd20LiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sqd20LiveProjectApplication.class, args);
+	}
+
+}
