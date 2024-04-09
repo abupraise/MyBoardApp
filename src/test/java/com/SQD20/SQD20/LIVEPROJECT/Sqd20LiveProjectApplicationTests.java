087460049dfcf54ff9e5fbfe66939d97c0bdb2aa
mac <ojoo@decagonhq.com>
2024-04-09 14:08:49 +0100

    removed @SppringBootTest

--- src/test/java/com/SQD20/SQD20/LIVEPROJECT/Sqd20LiveProjectApplicationTests.java
+++ src/test/java/com/SQD20/SQD20/LIVEPROJECT/Sqd20LiveProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.SQD20.SQD20.LIVEPROJECT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Sqd20LiveProjectApplicationTests {
 
 	@Test
