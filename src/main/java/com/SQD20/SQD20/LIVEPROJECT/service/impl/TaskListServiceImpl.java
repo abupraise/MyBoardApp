ea49e13c6c6e067a242fac4dff054e9a350ceb27
evaloper <gbajeeva@yahoo.com>
2024-04-15 23:50:36 +0100

    Revert back to the default response

--- src/main/java/com/SQD20/SQD20/LIVEPROJECT/service/impl/TaskListServiceImpl.java
+++ src/main/java/com/SQD20/SQD20/LIVEPROJECT/service/impl/TaskListServiceImpl.java
@@ -37,11 +37,7 @@ public class TaskListServiceImpl implements TaskListService {
             taskList.setDescription(taskListRequest.getDescription()); // Update the description field of the fetched TaskList entity
         }
 
-        TaskList taskListObj = new TaskList();
-        taskListObj.setTitle(taskList.getTitle());
-        taskListObj.setDescription(taskList.getDescription());
-
-        return taskListObj; // Save the updated TaskList entity
+        return taskListRepository.save(taskList); // Save the updated TaskList entity
     }
 
 }
